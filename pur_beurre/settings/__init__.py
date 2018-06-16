e60efd476c828c65908e163e743491ef0fcd49cf
jfsubrini <jfsubrini@zoho.com>
2018-06-16 21:10:14 +0200

    local env

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #### A VIRER
 ]
 
 ROOT_URLCONF = 'pur_beurre.urls'
