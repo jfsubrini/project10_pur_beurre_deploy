48a4b06e5e788940296bfc1f2198f8d0f3f2a8e6
jfsubrini <jfsubrini@zoho.com>
2018-06-20 14:05:54 +0200

    Static_root in settings.

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 # Redirection url when logged in
